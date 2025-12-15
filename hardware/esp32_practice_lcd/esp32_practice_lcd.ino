c3b2015737420ae5b4708cc2aca2d40d1a81c2b9
Okhotsk-hr <hrkiha183.service@gmail.com>
2025-12-15 21:34:32 +0900

    カタカナ表示

--- hardware/esp32_practice_lcd/esp32_practice_lcd.ino
+++ hardware/esp32_practice_lcd/esp32_practice_lcd.ino
@@ -1,18 +1,24 @@
-/* ESP32でLCDに文字表示 
- *
- */
- 
- #include <LiquidCrystal.h>
+/* ESP32でLCDに文字表示（半角カタカナ対応） */
 
-LiquidCrystal lcd(32,25,26,27,14,13);
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(32, 25, 26, 27, 14, 13);
 
 void setup()
 {
     lcd.begin(16, 2);
     lcd.clear();
-    lcd.print("Hello World!");
-    lcd.setCursor(0,1); 
-    lcd.print ("LCD with ESP32");
+
+    lcd.print("3.14");
+
+    lcd.setCursor(0, 1);
+
+    // 「ｶﾀｶﾅ」を半角カタカナで表示
+    lcd.write(0xB6); // ｶ
+    lcd.write(0xC0); // ﾀ
+    lcd.write(0xB6); // ｶ
+    lcd.write(0xC5); // ﾅ
 }
+
 void loop() {
 }
