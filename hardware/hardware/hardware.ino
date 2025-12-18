b0bc79b7f108bdf2921ebba9eac3f670f4ea03fc
Okhotsk-hr <hrkiha183.service@gmail.com>
2025-12-19 01:22:02 +0900

    ハードウェア単体の動作プログラム完了(実機調整これから)

--- hardware/hardware/hardware.ino
+++ hardware/hardware/hardware.ino
@@ -1,8 +1,9 @@
-#define SW1 33
-#define SW2 33
+#define SW1 22
+#define SW2 23
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(32,25,26,27,14,13);
+LiquidCrystal lcd(32, 25, 26, 27, 14, 13);
 
+//質問の配列
 char Q[4][50] = {
   "boy or girl",
   "new or old",
@@ -10,60 +11,52 @@ char Q[4][50] = {
   "ONIGIRI or Hamburger"
 };
 
-int inputlog[3];
+//回答を保存するための文字列
+char ans = "";
+
+//ボタンの状態を保存する変数
+int state1 = 1;
+int state2 = 1;
 
 void setup() {
+  //シリアルモニターを使用するため
   Serial.begin(9600);
+  //ボタンの設定
   pinMode(SW1, INPUT_PULLUP);
   pinMode(SW2, INPUT_PULLUP);
 
+  //LCDを使用するため
   lcd.begin(16, 2);
-  /*
-  lcd.clear();
-  lcd.print("Hello World!");
-  lcd.setCursor(0,1); 
-  lcd.print ("LCD with ESP32");
 
-  //文字列追加
-  snprintf(buf,sizeof(buf),"%s","ア");
-  Serial.println(buf);
-  */
+  //質問の数繰り返し
+  for (int i = 0; i < 4; i++) {
+    //2行目に質問の選択肢を表示
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print(Q[i]);
 
-  
+    //ボタンの状態を変数に保存
+    state1 = digitalRead(SW1);
+    state2 = digitalRead(SW2);
 
-  for(int i=0;i<5;i++){
-    int lastState1 = HIGH;
-    int lastState2 = HIGH;
-
-    while(true){
-      int currentState1 = digitalRead(SW1);
-      int currentState2 = digitalRead(SW2);
-
-      lcd.clear();
-      lcd.setCursor(0, 1);
-      lcd.print(Q[i]);
-
-      if(lastState1==HIGH && currentState1==LOW){
-        inputlog[i] = 0;
-        break;
-      }else if(lastState2==HIGH && currentState2==LOW){
-        inputlog[i] = 1;
-        break;
-      }
-
-      lastState1 = currentState1;
-      lastState2 = currentState2;
-      delay(50);
+    //ボタンがどちらか押されるまで先に進まない
+    while (!state1 && !state2) {
+      //ボタンの状態更新
+      state1 = digitalRead(SW1);
+      state2 = digitalRead(SW2);
     }
+
+    //ボタン1が押されたら0,ボタン2が押されたら1で回答を保存
+    if (!state1) {
+      ans += "0";
+    } else {
+      ans += "1";
+    }
+    //確認用にシリアルモニターに出力
+    Serial.println(ans);
   }
 }
 
 void loop() {
-  /*
-  int state1 = digitalRead(SW1);
-  Serial.println(state1);
-  int state2 = digitalRead(SW2);
-  Serial.println(state2);
-  */
+  //loopは使わない
 }
-
