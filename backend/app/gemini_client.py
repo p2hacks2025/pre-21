6984c83374941952db588f90fd4618821cce4771
nigehazi_kizoku <1111pocky.rei@gmail.com>
2025-12-19 01:31:15 +0900

    gemini_clientで使用するライブラリの変更
    下記のライブラリを追加
    from typing import List, Optional
    from pydantic import BaseModel, Field
    import os  # 追加
    from dotenv import load_dotenv  # 追加
    
    下記を変更
    変更前
    google-genai
    from google.genai import types
    from fastapi import HTTPException
    変更後
    google.generativeai
    from fastapi import FastAPI, HTTPException, Query
    
    ライブラリのソート
    
    環境変数の指定

--- backend/app/gemini_client.py
+++ backend/app/gemini_client.py
@@ -1,9 +1,15 @@
-from fastapi import HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from google import genai
 from google.genai import types
-from .config import settings
+from config import settings
 
+from typing import List, Optional
+from pydantic import BaseModel, Field
+import os  # 追加
+from dotenv import load_dotenv  # 追加
 
+load_dotenv() # .envファイルを読み込む
+API_KEY = os.getenv("GOOGLE_API_KEY")
 
 if not API_KEY:
     raise RuntimeError("環境変数 GOOGLE_API_KEY が設定されていません。")
@@ -13,6 +19,7 @@ MODEL_NAME = "gemini-3.0-flush"
 
 
 client = genai.Client(api_key=API_KEY)
+
 
 def send_text():
     pass
