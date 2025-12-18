7beeb8a502c1b84bf50818ae01cb065c3ceeba80
sugimoto-blyukher <143075979+sugimoto-blyukher@users.noreply.github.com>
2025-12-19 02:19:05 +0900

    print_service.pyを更新

--- backend/app/print_service.py
+++ backend/app/print_service.py
@@ -1,16 +1,12 @@
-from pathlib import Path
 import subprocess
 
-def print_service(pdf_path : str):
-    here = Path(__file__).resolve().parent        # .../backend/app
-    script = (here / ".." / "scripts" / "print_pdf.sh").resolve()  # .../backend/scripts/test.sh
+def print_pdf(copies: str) -> None:
+    cmd = ["bash", "scripts/print_pdf.sh", "data/artifacts" + "/" + f"{copies}.pdf"]
 
-    pdf = Path(pdf_path).resolve()
+    print(cmd)
 
-    if not script.exists():
-        raise FileNotFoundError(f"script not found: {script}")
+    subprocess.run(cmd, check=True, text=True, capture_output=True)
 
-    subprocess.run(["bash", str(script), str(pdf)], check=True)
 
 if __name__ == "__main__":
-    print_service("./data/artifacts/1.pdf")
+    print_pdf("1")
